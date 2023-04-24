b0b3a4d20ade95b1479f5e3e6d2ba47a28f1a517
Ziv Morgan <ziv.morgan@gmail.com>
2023-04-24 13:12:11 +0300

    fast layers

--- assignment2/CV7062610/fast_layers.py
+++ assignment2/CV7062610/fast_layers.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import numpy as np
+
 
 try:
     from CV7062610.im2col_cython import col2im_cython, im2col_cython
@@ -12,29 +12,19 @@ except ImportError:
 
 from CV7062610.im2col import *
 
-import cupy as cp
+import numpy as cp
 from numba import jit, prange
 
 
 
-    
-        
-
 def cov(x_pad, w, b,stride,out):
-    
-    
     for o_H in range(out.shape[2]):
         for o_W in range(out.shape[3]): 
             out[:,:,o_H,o_W] = cp.einsum('nchw, fchw -> nf',x_pad[:,:,stride*o_H:stride*o_H+w.shape[2],stride*o_W:stride*o_W+w.shape[3]],w)
-    
     for F in range(out.shape[1]):
         out[:,F,:,:] = out[:,F,:,:] + b[F] 
     return out
         
-        
-            
-            
-
 def conv_forward_strides(x, w, b, conv_param):
     N, C, H, W = x.shape
     F, _, HH, WW = w.shape
@@ -46,36 +36,24 @@ def conv_forward_strides(x, w, b, conv_param):
 
     # Pad the input
     p = pad
-    x_padded = np.pad(cp.asnumpy(x), ((0, 0), (0, 0), (p, p), (p, p)), mode="constant")
+    x_padded = cp.pad(cp.array(x), ((0, 0), (0, 0), (p, p), (p, p)), mode="constant")
 
     # Figure out output dimensions
     H += 2 * pad
     W += 2 * pad
     out_h = (H - HH) // stride + 1
     out_w = (W - WW) // stride + 1
-
-    # Perform an im2col operation by picking clever strides
     
     out = cp.zeros((N,F,out_h,out_w))
     out = cov(cp.array(x_padded), cp.array(w), cp.array(b), stride,out)
-    out = cp.asnumpy(out)
+    
+    #for using cupy
+    #out = cp.asnumpy(out)
 
     
 
     cache = (x_padded, w, b, conv_param)#, x_cols)
     return out, cache
-
-
-
-
-import tensorflow as tf
-import numpy as np
-
-
-
-
-import numpy as np
-from numba import jit, prange
 
 
 def conv_backward_strides(dout,cache):
@@ -97,33 +75,40 @@ def conv_backward_strides(dout,cache):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    x_pad,w,b,conv_param = cache
+    x_pad,w,b,conv_param =cache
+    x_pad = cp.array(x_pad)
+    w = cp.array(w)
+    b = cp.array(b)
+    
     pad = conv_param["pad"]
     stride = conv_param["stride"]
+    
     x = x_pad 
     if pad>0:
         x=x[:,:,pad:-pad,pad:-pad]
 
-    dw = np.zeros_like(w)
-    dx = np.zeros_like(x)
-    db = np.zeros_like(b)
+    dw = cp.zeros_like(w)
+    dx = cp.zeros_like(x)
+    
+    for HH in range(dw.shape[2]):
+        for WW in range(dw.shape[3]): 
+            dw[:,:,HH,WW] = cp.einsum('nchw, nfhw -> fc',x_pad[:,:,HH:HH+dout.shape[2]*stride:stride,WW:WW+dout.shape[3]*stride:stride],dout)
 
     
-    for F in prange(w.shape[0]):
-        for N in range(dout.shape[0]):
-            
-            for H in range(dout.shape[2]):
-                for W in range(dout.shape[3]):
-                    for C in range(w.shape[1]):
-                        for HH in range( w.shape[2]):
-                            for WW in range(w.shape[3]):
-                            
-                                dw[F,C,HH,WW] = dw[F,C,HH,WW] + dout[N,F,H,W]*x_pad[N,C,H*stride + HH,W*stride + WW]
-                                
-                                if HH + H*stride - pad >= 0 and HH + H*stride  - pad < x.shape[2] and WW + W*stride - pad >= 0 and WW + W*stride - pad < x.shape[3]:
-                                    dx[N,C,H*stride+HH - pad,W*stride+WW - pad] = dx[N,C,H*stride+HH - pad,W*stride+WW - pad] + dout[N,F,H,W]*w[F,C, HH, WW]
-                    db[F] = db[F]+dout[N,F,H,W]
+    dx_pad = cp.pad(dx, ((0, 0), (0, 0), (pad, pad), (pad, pad)), mode="constant")
+    
+    for H in range(0,dout.shape[2]):
+        for W in range(0,dout.shape[3]):
+            dx_pad[:,:,H*stride:H*stride+w.shape[2], W*stride:W*stride+w.shape[3]] += cp.einsum('nf, fchw -> nchw',dout[:,:,H,W],w)
 
+    if pad>0:
+        dx=dx_pad[:,:,pad:-pad,pad:-pad]
+    else:
+        dx = dx_pad
+    
+    db = cp.sum(dout, axis=(0, 2, 3))
+
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
     #                             END OF YOUR CODE                            #
@@ -132,8 +117,6 @@ def conv_backward_strides(dout,cache):
 
 
     
-
-
 
 conv_forward_fast = conv_forward_strides
 conv_backward_fast = conv_backward_strides
@@ -151,16 +134,19 @@ def max_pool_forward_fast(x, pool_param):
     N, C, H, W = x.shape
     pool_height, pool_width = pool_param["pool_height"], pool_param["pool_width"]
     stride = pool_param["stride"]
+    out = cp.zeros((N,C,1+(H-pool_height)//stride,1+(W-pool_width)//stride))
+    x = cp.array(x)
+    
+    for o_H in range(out.shape[2]):
+        for o_W in range(out.shape[3]):
+            out[:,:,o_H,o_W] = cp.max(cp.max(x[:,:,o_H*stride:o_H*stride + pool_height, o_W*stride:o_W*stride + pool_width],axis=2),axis=2)
+            
+            
 
-    same_size = pool_height == pool_width == stride
-    tiles = H % pool_height == 0 and W % pool_width == 0
-    if same_size and tiles:
-        out, reshape_cache = max_pool_forward_reshape(x, pool_param)
-        cache = ("reshape", reshape_cache)
-    else:
-        out, im2col_cache = max_pool_forward_im2col(x, pool_param)
-        cache = ("im2col", im2col_cache)
-    return out, cache
+    
+    #out = cp.asnumpy(out)
+    #x = cp.asnumpy(x)
+    return out, (x,pool_param)
 
 
 def max_pool_backward_fast(dout, cache):
@@ -170,11 +156,19 @@ def max_pool_backward_fast(dout, cache):
     This switches between the reshape method an the im2col method depending on
     which method was used to generate the cache.
     """
-    method, real_cache = cache
-    if method == "reshape":
-        return max_pool_backward_reshape(dout, real_cache)
-    elif method == "im2col":
-        return max_pool_backward_im2col(dout, real_cache)
-    else:
-        raise ValueError('Unrecognized method "%s"' % method)
+    x, pool_param = cache
+    x = cp.array(x)
+    dx = cp.zeros_like(x)
+    pH = pool_param['pool_height']
+    pW = pool_param['pool_width']
+    stride = pool_param['stride']
+    
+    for H in range(dout.shape[2]):
+        for W in range(dout.shape[3]):
+            pool = x[:,:,H*stride:H*stride + pH, W*stride:W*stride + pW]
+                    
+            mask = (pool == cp.max(cp.max(pool, axis=3, keepdims=True),axis=2,keepdims=True))
+            dx[:,:,H*stride:H*stride+pH,W*stride:W*stride+pW]+= mask*dout[:,:,H,W][:,:,cp.newaxis,cp.newaxis]
+
+    return dx
 
